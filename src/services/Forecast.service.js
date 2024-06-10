19bbabd1eee9339b8d5746322e9a537b8f71fa71
Ian Garraway <IJGarraway@gmail.com>
2024-06-10 18:32:42 +0100

    Feat: add code utility to reduce forecast data to just what is needed

--- src/services/Forecast.service.js
+++ src/services/Forecast.service.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import WeatherDataConverter from "../utils/WeatherDataConverter.js";
 
 export default class ForecastService{
      getForecast = async (location) => {
@@ -6,6 +7,6 @@ export default class ForecastService{
         const { OPENWEATHERKEY: apiKey } = process.env;        
         let url = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&cnt=40&appid=${apiKey}`        
         let response = await axios.get(url);
-        return response.data;            
+        return WeatherDataConverter(response.data);            
     }
 }
